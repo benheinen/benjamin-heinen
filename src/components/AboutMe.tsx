5abefb4617ab3c25c2f4ad37aa31986bdf6664e8
Ben Heinen <benbuis24@gmail.com>
2024-06-20 16:55:05 -0500

    added grid template

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,41 +2,60 @@ import {
   Box,
   Button,
   GridItem,
-  Heading,
-  SimpleGrid,
   Text,
   Image,
+  Grid,
+  VStack,
+  HStack,
+  Spacer,
 } from "@chakra-ui/react";
+import CategoryHeading from "./CategoryHeading";
 
 export default function AboutMe() {
   return (
     <Box>
-      <Heading size={"xl"}>About Me</Heading>
-      <SimpleGrid columns={2} paddingInline={10}>
-        <GridItem>
+      <Grid
+        templateAreas={`"heading heading"
+            "image text"`}
+      >
+        <GridItem area={"heading"}>
+          <CategoryHeading
+            title={"About Me"}
+            subtitle={"I like to make things"}
+          ></CategoryHeading>
+        </GridItem>
+        <GridItem area={"image"}>
           <Image
             borderRadius={"xl"}
             src="https://via.placeholder.com/300"
           ></Image>
         </GridItem>
-        <GridItem>
-          <Text>
-            Hello! My Name is Ben and I'm an aspiring software engineer studying
-            computer science at the University of Minnesota, Twin Cities.
-          </Text>
-          <Text>
-            I am increasingly inspired by the innovative technologies being
-            developed around us at all times. My goal is to contribute to this
-            innovation with practical solutions.
-          </Text>
-          <Text>
-            In addition to programming, I enjoy competing in Ironman races and
-            Ultramarathons. I find it important to maintain a healthy lifestyle
-            so I can provide my best work.
-          </Text>
-          <Button colorScheme={"orange"}>Resume</Button>
+        <GridItem area={"text"} paddingInline={10}>
+          <VStack spacing={5} mb={5}>
+            <Text>
+              Hello! My Name is Ben and I'm an aspiring software engineer
+              studying computer science at the University of Minnesota, Twin
+              Cities.
+            </Text>
+            <Text>
+              I am increasingly inspired by the innovative technologies being
+              developed around us at all times. My goal is to contribute to this
+              innovation with practical solutions.
+            </Text>
+            <Text>
+              In addition to programming, I enjoy competing in Ironman races and
+              Ultramarathons. I find it important to maintain a healthy
+              lifestyle so I can provide my best work.
+            </Text>
+          </VStack>
+          <HStack>
+            <Spacer></Spacer>
+            <Button size={"md"} colorScheme={"orange"}>
+              Resume
+            </Button>
+          </HStack>
         </GridItem>
-      </SimpleGrid>
+      </Grid>
     </Box>
   );
 }
