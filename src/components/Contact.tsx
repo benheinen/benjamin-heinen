4a1987500ce7d61d44216421eb5a1763a0eedf72
Ben Heinen <benbuis24@gmail.com>
2024-06-20 18:24:41 -0500

    new component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,5 @@
+import CategoryHeading from "./CategoryHeading";
+
+export default function Contact() {
+  return <CategoryHeading title={"Message"} subtitle={"Any questions?"} />;
+}
---
+++ src/components/Projects.tsx
@@ -0,0 +1,10 @@
+import CategoryHeading from "./CategoryHeading";
+
+export default function Projects() {
+  return (
+    <CategoryHeading
+      title={"Projects"}
+      subtitle={"My recent ideas come to life!"}
+    />
+  );
+}
