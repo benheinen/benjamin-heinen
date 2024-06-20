0227322b01a1fe833114ad4d6f71799466b71f41
Ben Heinen <benbuis24@gmail.com>
2024-06-20 15:32:33 -0500

    added about me

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,51 @@
 import { EmailIcon } from "@chakra-ui/icons";
-import { Box, Heading, Text, Button, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Text,
+  Button,
+  HStack,
+  Flex,
+  Image,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Box bg="gray.300">
-      <Box marginInline={200}>
+    <Box bg="gray.300" paddingInline={200}>
+      <Box paddingBlock={150}>
         <Heading size={"4xl"}>Hi, I'm Ben!</Heading>
         <Heading size={"lg"}>Aspiring Software Engineer</Heading>
         <Text marginBlock={5}>
-          Currently pursuing a Computer Science degree <br /> @ the University
-          of Minnesota - Twin Cities
+          Currently pursuing a Computer Science degree <br />
+          <Text as="em" fontWeight={"bold"}>
+            @ the University of Minnesota - Twin Cities
+          </Text>
         </Text>
         <HStack>
-          <Button colorScheme={"blue"} leftIcon={<EmailIcon />}>
-            Message
+          <Button size={"lg"} colorScheme={"orange"} leftIcon={<EmailIcon />}>
+            Contact
           </Button>
-          <Button colorScheme={"gray"}></Button>
         </HStack>
+      </Box>
+      <Box>
+        <Heading size={"xl"}>About Me</Heading>
+        <Flex>
+          <Image src="https://via.placeholder.com/300"></Image>
+          <Text>
+            Hello! My Name is Ben and I'm an aspiring software engineer studying
+            computer science at the University of Minnesota, Twin Cities.
+          </Text>
+          <Text>
+            I am increasingly inspired by the innovative technologies being
+            developed around us at all times.
+          </Text>
+          <Text>
+            In addition to programming, I enjoy competing in Ironman races and
+            Ultramarathons. I find it important to maintain a healthy lifestyle
+            so I can provide my best work.
+          </Text>
+          <Button>Resume</Button>
+        </Flex>
       </Box>
     </Box>
   );
