1c3680973b2d229facb26ea0e6648df54797b50e
Ben Heinen <benbuis24@gmail.com>
2024-06-20 19:40:35 -0500

    added footer

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function RootLayout() {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
