8e7c6ce6209709db2ae4b8a906572d5e24654874
Ben Heinen <benbuis24@gmail.com>
2024-06-20 19:11:23 -0500

    redesign

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,43 @@
+import {
+  Button,
+  HStack,
+  Heading,
+  VStack,
+  Image,
+  Text,
+  Box,
+} from "@chakra-ui/react";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <VStack
+      borderTop="0.5px"
+      borderTopColor={"gray.400"}
+      borderTopStyle={"solid"}
+    >
+      <VStack gap={2} marginBlock={5}>
+        <Heading color={"gray.700"} size={"lg"}>
+          Benjamin Heinen
+        </Heading>
+        <HStack gap={5}>
+          <Button variant={"link"}>About</Button>
+          <Button variant={"link"}>Projects</Button>
+          <Button variant={"link"}>Updates</Button>
+          <Button variant={"link"}>Contact</Button>
+        </HStack>
+      </VStack>
+      <HStack>
+        <Box bg="gray.800" p={1} borderRadius={"lg"}>
+          <Image boxSize="20px" src="github-white.png" alt="GitHub Logo" />
+        </Box>
+        <Box bg="gray.800" p={1} borderRadius={"lg"}>
+          <Image boxSize="20px" src="instagram-white.png" alt="GitHub Logo" />
+        </Box>
+        <Box bg="gray.800" p={1} borderRadius={"lg"}>
+          <Image boxSize="20px" src="linkedin-white.png" alt="GitHub Logo" />
+        </Box>
+      </HStack>
+      <Text fontSize={"sm"}>Developed by Ben Heinen</Text>
+    </VStack>
+  );
 }
