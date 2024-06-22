68fee5c25ae01ccc65e91037e9b947afb904d3d3
Ben Heinen <benbuis24@gmail.com>
2024-06-21 19:16:10 -0500

    added scroll to top button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,15 +14,17 @@ import TechnicalSkills from "../components/TechnicalSkills";
 import AboutMe from "../components/AboutMe";
 import Projects from "../components/Projects";
 import Connect from "../components/Connect";
+import BackToTopButton from "../components/BackToTopButton";
 
 export default function Home() {
   return (
     <Box bg="gray.200" paddingInline={250}>
+      <BackToTopButton></BackToTopButton>
       <Grid paddingBlock={150} templateAreas={`"greeting image"`}>
         <GridItem area={"greeting"}>
           <Heading size={"4xl"}>Hi, I'm Ben!</Heading>
           <Heading size={"lg"}>Aspiring Software Engineer</Heading>
-          <Text marginBlock={5}>
+          <Text marginBlock={5} fontSize={"lg"}>
             Currently pursuing a Computer Science degree <br />
             <Text as="em" fontWeight={"bold"}>
               @ the University of Minnesota - Twin Cities
@@ -41,7 +43,7 @@ export default function Home() {
         </GridItem>
         <GridItem area="image"></GridItem>
       </Grid>
-      <VStack gap={175} mb={175}>
+      <VStack gap={190} paddingBottom={125}>
         <AboutMe />
         <TechnicalSkills />
         <Projects />
